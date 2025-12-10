28b5e121f32b9a08ebdc58b3c8d4cd8f751c1cee
Kimhyeungyeum <vjsclgud@gmail.com>
2025-12-10 20:45:05 +0900

    1210

---
+++ 16.좋은 수 찾기.py
@@ -0,0 +1,35 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+A = list(map(int, input().split()))
+
+A.sort()
+
+srt = 0
+end = 0
+count = 0
+
+for k in range(N):
+    find = A[k]
+    start = 0
+    end = N - 1
+    while start < end:
+        if A[start] + A[end] == find:
+            if start != k and end != k:
+                count += 1
+                break
+
+            elif start == k:
+                start += 1
+
+            elif end == k:
+                end -= 1
+
+        elif A[start] + A[end] < find:
+            start += 1
+        else:
+            end -= 1
+
+
+print(count)
